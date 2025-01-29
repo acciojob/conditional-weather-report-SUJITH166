7412bbaf9d513b80b503159fed3eba5b9ecb8697
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-01-29 16:33:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
-
+const def = {temperature : 0, conditions : "Cool"}
 const App = () => {
+
+  const[info, setInfo] = useState(def)
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setInfo({ temperature: 25, conditions: "Sunny" })
+    }, 2000)
+  },[])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay info={info} />
     </div>
   )
 }
